3080be36ba68ba67fabab09a95af0c676cde3eff
jfeuillatre <jfeuillatre@jfeuillatre.home>
2024-03-01 11:51:02 +0100

    clean + formatted

--- markov_chain.py
+++ markov_chain.py
@@ -7,6 +7,20 @@ class MarkovChain:
         self.words = {}
         self.starts = {}
 
+    def _update_starts(self, word: str):
+        if word not in self.starts:
+            self.starts[word] = 1
+        else:
+            self.starts[word] += 1
+
+    def _update_words(self, word: str, next_w: str | None):
+        if word not in self.words:
+            self.words[word] = {}
+        if next_w not in self.words[word]:
+            self.words[word][next_w] = 1
+        else:
+            self.words[word][next_w] += 1
+
     def space_ponctuation(self, text: str):
         for k in ".,!?":
             text = text.replace(f" {k}", f"{k}")
@@ -22,39 +36,16 @@ class MarkovChain:
         for line in self.generator_clean_text(text):
             if not line:
                 continue
-            if line[0] not in self.starts:
-                self.starts[line[0]] = 1
-            else:
-                self.starts[line[0]] += 1
-
+            self._update_starts(line[0])
             prev_w = None
             for i in range(len(line) - 1):
-                if line[i] not in self.words:
-                    self.words[line[i]] = {}
-                if line[i + 1] not in self.words[line[i]]:
-                    self.words[line[i]][line[i + 1]] = 1
-                else:
-                    self.words[line[i]][line[i + 1]] += 1
-
-                if prev_w and prev_w + "+" + line[i] not in self.words:
-                    self.words[prev_w + "+" + line[i]] = {}
-                if prev_w and line[i + 1] not in self.words[prev_w + "+" + line[i]]:
-                    self.words[prev_w + "+" + line[i]][line[i + 1]] = 1
-                elif prev_w:
-                    self.words[prev_w + "+" + line[i]][line[i + 1]] += 1
+                self._update_words(line[i], line[i + 1])
+                if prev_w:
+                    self._update_words("+".join([prev_w, line[i]]), line[i + 1])
                 prev_w = line[i]
-
-            if line[-1] not in self.words:
-                self.words[line[-1]] = {}
-                self.words[line[-1]][None] = 1
-            else:
-                self.words[line[-1]][None] += 1
-
-            if len(line) > 1 and line[-2] + line[-1] not in self.words:
-                self.words[line[-2] + "+" + line[-1]] = {}
-                self.words[line[-2] + "+" + line[-1]][None] = 1
-            elif len(line) > 1:
-                self.words[line[-2] + "+" + line[-1]][None] += 1
+            self._update_words(line[-1], None)
+            if len(line) > 1:
+                self._update_words("+".join([line[-2], line[-1]]), None)
 
     def generate_text(self, seed: int = None, max_len: int = 20):
         text = []
@@ -62,40 +53,24 @@ class MarkovChain:
             random.seed(seed)
         curr = random.choices(
             population=list(self.starts.keys()),
-            weights=[float(self.starts[k] / len(self.starts)) for k in list(self.starts.keys())],
+            weights=[float(self.starts[k] / len(self.starts)) for k in self.starts],
         )[0]
         text.append(curr)
         prev_w = None
         for _ in range(max_len):
             tmp = curr
-            if prev_w and prev_w + "+" + curr in list(self.words.keys()):
-                sum_weights = 0
-                for k in self.words[prev_w + "+" + curr].keys():
-                    sum_weights += self.words[prev_w + "+" + curr][k]
-                curr = random.choices(
-                    population=list(self.words[prev_w + "+" + curr].keys()),
-                    weights=[
-                        float(self.words[prev_w + "+" + curr][k] / sum_weights)
-                        for k in list(self.words[prev_w + "+" + curr].keys())
-                    ],
-                )[0]
-                if not curr:
-                    break
-            else:
-                if curr not in list(self.words.keys()):
-                    break
-                sum_weights = 0
-                for k in self.words[curr].keys():
-                    sum_weights += self.words[curr][k]
-                curr = random.choices(
-                    population=list(self.words[curr].keys()),
-                    weights=[
-                        float(self.words[curr][k] / sum_weights)
-                        for k in list(self.words[curr].keys())
-                    ],
-                )[0]
-                if not curr:
-                    break
+            curr_w = "+".join(filter(None, [prev_w, curr]))
+            if curr_w not in self.words:
+                break
+            sum_weights = 0
+            for k in self.words[curr_w]:
+                sum_weights += self.words[curr_w][k]
+            curr = random.choices(
+                population=list(self.words[curr_w].keys()),
+                weights=[float(self.words[curr_w][k] / sum_weights) for k in self.words[curr_w]],
+            )[0]
+            if not curr:
+                break
             text.append(curr)
             prev_w = tmp
         generated_text = " ".join(text)
